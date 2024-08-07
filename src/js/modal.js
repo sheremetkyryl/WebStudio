3b37d5eb211e0dd769bea380a34eb5e1def7b890
kruisher <kruisher@gmail.com>
2024-08-08 00:35:12 +0200

    finished with modal window and maked form in footer

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,17 +1,15 @@
 (() => {
-    const refs = {
-      openModal: document.querySelector("[data-modal-open]"),
-      closeModal: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModal.addEventListener("click", toggleModal);
-    refs.closeModal.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-      document.body.classList.toggle("no-scroll");
-    }
-  })();
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
 
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    document.body.classList.toggle("no-scroll");
+  }
+})();
